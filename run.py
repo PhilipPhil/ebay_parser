5f8dcf4c2c883e16ac697942e443a99b1620ea8b
PhilipPhil <philipgarabandic@gmail.com>
2020-11-14 15:47:56 +0000

    no message

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from Scraper import Scraper
-
-scraper = Scraper()
-# for data
-scraper.add_books('1449690777', 100)
-scraper.send_email()
+# run every 2 hours
+    scraper = Scraper()
+    # for data in db
+        scraper.add_books('1449690777', 100)
+    scraper.send_email()

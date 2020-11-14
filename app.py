049dbddc83c3a23a78f49dc33f19b666d5bf96bf
PhilipPhil <philipgarabandic@gmail.com>
2020-11-14 22:53:52 +0000

    no message

--- app.py
+++ app.py
@@ -13,27 +13,27 @@ db = SQLAlchemy(app)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-	if request.method == 'POST':
-		try:
-			files = request.files.getlist('files')
-			db.session.query(Search.Search).delete()
-			for f in files:
-				searches = pd.read_excel(f)
-				searches = pd.DataFrame(searches, columns=['book_id', 'max_price'])
-				for _, row in searches.T.iteritems():
-					new_search = Search.Search(book_id=row['book_id'], max_price=row['max_price'])
-					db.session.add(new_search)
-			db.session.commit()
-            # scraper.reset_urls_sent()
-			return '<h1 style="text-align:center">Upload successful</h1>'
-		except:
-			return '<h1 style="text-align:center">There was an issue adding your files.</h1>'
+    if request.method == 'POST':
+        try:
+            files = request.files.getlist('files')
+            db.session.query(Search.Search).delete()
+            for f in files:
+                searches = pd.read_excel(f)
+                searches = pd.DataFrame(searches, columns=['book_id', 'max_price'])
+                for _, row in searches.T.iteritems():
+                    new_search = Search.Search(book_id=row['book_id'], max_price=row['max_price'])
+                    db.session.add(new_search)
+            db.session.commit()
+            scraper.reset_urls_sent()
+            return '<h1 style="text-align:center">Upload successful</h1>'
+        except:
+            return '<h1 style="text-align:center">There was an issue adding your files.</h1>'
 
-	else:
-		return render_template('index.html')
+    else:
+        return render_template('index.html')
 
 
 if __name__ == "__main__":
-	scraper_thread = threading.Thread(target=scraper.run)
-	scraper_thread.start()
-	app.run(debug=True)
+    scraper_thread = threading.Thread(target=scraper.run)
+    scraper_thread.start()
+    app.run(debug=True)

5790954d68655e2f9755168f9c3c7a9581c243f0
gracia-gu <gracia9828@hotmail.com>
2020-11-23 18:56:29 +0000

    add shipping cost

--- Scraper.py
+++ Scraper.py
@@ -6,8 +6,7 @@ from app import Search
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from IDAPP import ID_APP
-from BannedSellers import BannedSellers
+
 
 class Scraper:
 
@@ -15,12 +14,11 @@ class Scraper:
     password_mail = 'sfoxktdmsbauccqa'
     # to_mail = 'sethbaker51@gmail.com'
     to_mail = 'gracia9828@gmail.com'
+    ID_APP = ['SethBake-ASINAler-PRD-47c01d8ca-e867093d','TheKaize-ASINAler-PRD-12eb4905c-db637f64']
 
     def __init__(self):
         self.urls_sent = set()
         self.ID_Index = 1
-        self.ID_APP = ID_APP
-        self.api_time_delay = max(0.3, 24*60*60/(5000*len(self.ID_APP)) + 0.05)
 
     def get_ID_Index(self):
         self.ID_Index+=1
@@ -32,24 +30,31 @@ class Scraper:
         index = self.get_ID_Index()
         print(self.ID_APP[index])
         api = finding(appid=self.ID_APP[index], config_file=None)
-        time.sleep(self.api_time_delay)
+        time.sleep(8.7)
         api_request = {'keywords': book_id,
                        'itemFilter': [
                            {'name': 'MaxPrice', 'value': max_price, 'paramName': 'Currency', 'paramValue': 'USD'},
                            {'name': 'ExcludeSeller',
-                            'value': BannedSellers}
+                            'value': ['fortwaynegoodwill', 'lazilyround', 'benjkuzn_0', 'integritybooksales',
+                                      'selectdiscountshop', 'discountshelf']}
                        ]}
         response = api.execute('findItemsAdvanced', api_request)
         soup = BeautifulSoup(response.content,'lxml')
         items = soup.find_all('item')
+        # print(items)
 
         books = []
         for item in items:
             price = float(item.currentprice.string)
+            if item.shippingservicecost is not None:
+                shipping = float(item.shippingservicecost.string)
+            else:
+                shipping = 'Unknown'
             title = item.title.string.lower()
             url = item.viewitemurl.string.lower()
             book_xml = item
-            book = Book(book_id, max_price, price, title, url, book_xml)
+
+            book = Book(book_id, max_price, price, shipping, title, url, book_xml)
             books.append(book)
         return books
 
@@ -62,6 +67,7 @@ class Scraper:
                     self.send_email(book)
 
     def send_email(self, book):
+
         server = smtplib.SMTP('smtp.gmail.com', 587)
         server.starttls()
         server.login(self.from_mail, self.password_mail)
@@ -70,16 +76,17 @@ class Scraper:
         msg['Subject'] = 'Book Alert'
         msg['From'] = self.from_mail
         msg['To'] = self.to_mail
-
         if book.url not in self.urls_sent:
             self.urls_sent.add(book.url)           
             html_mail = self.email_html(book)
             text_xml = book.book_xml.prettify()
+
             msg.attach(MIMEText(html_mail, 'html'))
             msg.attach(MIMEText(text_xml, 'plain')) 
-            server.sendmail(self.from_mail, self.to_mail, msg.as_string())
-            print('url: ' + book.url)
 
+            server.sendmail(self.from_mail, self.to_mail, msg.as_string())
+            
+            print('url: ' + book.url)
         server.quit()
 
     def email_html(self, book):
@@ -103,6 +110,7 @@ class Scraper:
                     <th>Title</th>
                     <th>Max Price</th>
                     <th>Price</th>
+                    <th>Shipping Cost</th>
                     <th>URL</th>
                 </tr>
                 <tr>
@@ -110,6 +118,7 @@ class Scraper:
                     <th>"""+book.title+ """</th>\
                     <th>"""+str(book.max_price)+"""</th>\
                     <th>"""+str(book.price)+"""</th>\
+                    <th>"""+str(book.shipping)+"""</th>\
                     <th>"""+book.url+"""</th>\
                 </tr>
             </table>
